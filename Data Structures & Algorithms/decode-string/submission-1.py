61b83d8a30cc175b6eef78ca32d292ac53ffebff
Jimmy <20414083+JayKay24@users.noreply.github.com>
2026-07-23 21:20:46 +0300

    Add: decode-string - submission-1

---
+++ Data Structures & Algorithms/decode-string/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        n = len(s)
+        num = 0
+        current = ""
+        stack: list[tuple[str, int]] = []
+
+        for i in range(n):
+            char = s[i]
+            if char.isdigit():
+                num = num * 10 + int(char)
+            elif char == "[":
+                stack.append((current, num))
+                num = 0
+                current = ""
+            elif char == "]":
+                prev, k = stack.pop()
+                current = prev + current * k
+            else:
+                current += char
+
+        return "".join(current)
